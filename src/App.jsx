a12cc2f36f2d4477f5338b1fd031bea901d5df7b
Daniel Vergaray <danielvergarayescobar@gmail.com>
2024-07-12 17:34:47 -0500

    avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { db } from "./data/db";
 
 function App() {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
 
+  const addToCart = (item) => {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+    if (itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart[updatedCart];
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !==id))
+  };
 
   return (
     <>
-  <Header/>
-      
+      <Header cart={cart} removeFromCart={removeFromCart} />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar/>
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
         </div>
       </main>
 
